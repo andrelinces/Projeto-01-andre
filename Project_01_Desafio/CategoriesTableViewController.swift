6678a2765f4aaa52ed4da4a5fbf2c97f96e6fbad
Andre Linces <andrelinces@yahoo.com.br>
2021-12-05 19:26:41 -0300

    Creates reference for alamofire API. - Retrived data from type Json. - Populating tableview - Reorganized code

--- Project_01_Desafio/CategoriesTableViewController.swift
+++ Project_01_Desafio/CategoriesTableViewController.swift
@@ -10,22 +10,43 @@ import Alamofire
 
 class CategoriesTableViewController: UITableViewController {
     
+    var responseArray: NSArray = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        AF.request("https://api.chucknorris.io/jokes/categories").responseJSON { response in
+            if let json = response.value {
+                            print("test json: \(json)")
+                            self.responseArray = json as! NSArray
+                print("test responseArray: \(self.responseArray)")
+                self.tableView.reloadData()
+                        }
+            
+        }
         
     }
-    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return responseArray.count
+        print("Test numberOfRowsInSection: \(responseArray)")
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseCategories", for: indexPath)
+       // let url = URL(string: "https://api.chucknorris.io/jokes/categories")
         
+        // Configure the cell...
+                let categories = responseArray[indexPath.row]
+                //let artistName = (whichCategories as AnyObject)["artistName"] as? String
         
+                cell.textLabel?.text = categories as? String
         
+        //        cell.textLabel?.text = [categories] as? String
+        //cell.textLabel?.text = "https://api.chucknorris.io/jokes/categories"
         
       return cell
     }
--- Project_01_Desafio/InitialViewController.swift
+++ Project_01_Desafio/InitialViewController.swift
@@ -37,28 +37,7 @@ class InitialViewController: UIViewController {
           let tappedImage = tapGestureRecognizer.view as! UIImageView
 
           performSegue(withIdentifier: "segueTableView", sender: nil)
-          
-          
-        
-        
-        
-        
-        
-        //let url = URL(string: "https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png")
-        // this downloads the image asynchronously if it's not cached yet
-        //initialButton.kf.setImage(Data)
-        
-//        let url = URL(string: image.url)
-//
-//        DispatchQueue.global().async {
-//            let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
-//            DispatchQueue.main.async {
-//                imageView.image = UIImage(data: data!)
-//            }
-//        }
-        
+         
     }
-    
-    
-    
+       
 }
